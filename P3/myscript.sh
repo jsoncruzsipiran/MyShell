43024cc39e16e9b6f8028b7998c265557424f8c3
jsoncruzsipiran <119018676+jsoncruzsipiran@users.noreply.github.com>
2025-11-10 16:27:24 -0500

    refactor: changed readFromStdIn() to be a main loop, since we must have 1 loop for both interactive and batch modes

---
+++ P3/myscript.sh
@@ -0,0 +1,10 @@
+# myscript.sh
+# A simple batch test script for mysh
+
+echo Starting batch test...
+pwd
+ls
+echo Moving up one directory...
+cd ..
+pwd
+echo Done!
--- P3/mysh.c
+++ P3/mysh.c
@@ -10,111 +10,111 @@
 #define BUFSIZE 4096
 #define WORDSIZE 16
 
-int readFromStdIn(){  // taken from PA2, but can be replaced later on
-    char buffer[BUFSIZE]; 
-    int bytes; 
-    int lineNumber = 0;
+/* welcome message for interactive mode */
+void printWelcome() { printf("Welcome to my shell!\n"); }
 
-    char *line = malloc(WORDSIZE);
-
-    if (!line)
-    {
-        fprintf(stderr, "Memory allocation failed in readFromStdIn\n");
-        return 1;
-    }
-
-    int capacity = WORDSIZE;
-    int lineIndex = 0;
-
-    while((bytes = read(STDIN_FILENO, buffer, BUFSIZE)) > 0) // loops while there are bytes to read
-    {
-        for(int i = 0; i < bytes; i++)
-        {
-            if(buffer[i] == '\n')
-            {
-                line[lineIndex] = '\0'; // end the string where newline is found
-                
-                lineNumber++;
-                
-                // HERE: replace with actual handler
-                printf("Processing: %s\n", line); // temp
-
-                lineIndex = 0;
-            } else {
-                if (lineIndex >= capacity - 1)
-                {
-                    capacity *= 2;
-
-                    char *temp = realloc(line, capacity);
-
-                    if (!temp)
-                    {
-                        fprintf(stderr, "Memory allocation failed while reading stdin\n");
-                        free(line);
-                        return 1;
-                    }
-
-                    line = temp;
-                }
-
-                line[lineIndex++] = buffer[i];
-            }
-        }
-    }
-
-    if (bytes < 0){
-        fprintf(stderr, "Error reading from stdin.\n");
-        return 1;
-    }
-
-    free(line);
-    return 0;
-}
-
-void printWelcome()
-{
-    printf("Welcome to my shell!\n");
-}
-
-void printGoodbye()
-{
-    printf("Exiting my shell.\n");
-}
+/* goodbye message for interactive mode */
+void printGoodbye() { printf("Exiting my shell.\n"); }
 
 int main(int argc, char *argv[]) {
     
+    /* checks for arguments over the necessary amount, must be 1 or none */
     if (argc > 2){
-
         fprintf(stderr, "Invalid number of arguments: %d. There should be at most 2 arguments.\n", argc); // can change later
         return 1;
-
     } 
 
+    /* batch mode is possible through 2 routes: program execution starts off with pipe redirecting STDIN or we are given an argument to redirect it manually */
     if(argc == 2)
     {
-        char *batchCommands = argv[1];
+        char *batchFile = argv[1]; // string to hold batch file
 
-        int fd = open(batchCommands, O_RDONLY);
+        int fd = open(batchFile, O_RDONLY); // file descriptor for batch file
         
+        /* print error if batch file did not successfully open */
         if(fd < 0)
         {
-            fprintf(stderr, "Error: could not open file %s\n", batchCommands);
+            fprintf(stderr, "Error: could not open file %s\n", batchFile);
             return EXIT_FAILURE;
         }
 
-        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDIN_FILENO); // link batchFile's fd to STDIN_FILENO, remove link between terminal and STDIN_FILENO
 
-        close(fd);
+        close(fd); // free link between the batchFile and its original fd
     }
 
-    int interactive = isatty(STDIN_FILENO);
+    int interactive = isatty(STDIN_FILENO); // interactive flag using isatty() method
 
+    /* print welcome message if we are interactive mode */
     if(interactive)
     {
         printWelcome();
         fflush(stdout);
     }
 
+    /* read from STDIN (may be terminal, may be batchFile) */
+    char buffer[BUFSIZE];
+    
+    char *commandLine = malloc(BUFSIZE);
+    if(!commandLine)
+    {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return EXIT_FAILURE;
+    }
+    
+    int bytes;
+    int lineIndex = 0;
+    int capacity = BUFSIZE;
+
+    while((bytes = read(STDIN_FILENO, buffer, BUFSIZE)) > 0)
+    {
+        for(int i = 0; i < bytes; i++)
+        {
+            if(buffer[i] == '\n')
+            {
+                if(lineIndex > 0)
+                {
+                    commandLine[lineIndex] = '\0';
+
+                    printf("%s\n", commandLine);
+
+                    lineIndex = 0;
+                }
+            } else {
+                if(lineIndex >= capacity - 1)
+                {
+                    capacity *= 2;
+
+                    char *temp = realloc(commandLine, capacity);
+                    if (!temp)
+                    {
+                        fprintf(stderr, "Memory reallocation failed.\n");
+                        free(commandLine);
+
+                        return EXIT_FAILURE;
+                    }
+
+                    commandLine = temp;
+                }
+
+                commandLine[lineIndex++] = buffer[i];
+            }
+        }
+    }
+
+    if (lineIndex > 0) 
+    {
+        commandLine[lineIndex] = '\0';
+        printf("%s\n", commandLine);
+    }
+
+    free(commandLine);
+
+    if(interactive)
+    {
+        printGoodbye();
+        fflush(stdout);
+    }
     
     return EXIT_SUCCESS;
 }
